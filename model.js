966565433edcfd9b06d8ea2ec34f514d83203d25
tutugodfrey <godfrey_tutu@yahoo.com>
2019-04-04 11:22:18 +0100

    install dependencies and setup server

---
+++ model.js
@@ -0,0 +1,5 @@
+import dummyDataModel from 'dummy-data-model';
+
+const users = new dummyDataModel('users', ['id', 'name', 'email']);
+
+export default users;
--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 import express from 'express';
 import express_graphql from 'express-graphql';
 import { buildSchema } from 'graphql';
+import users from './model';
 
 // graphql schema 
 const schema = buildSchema(`
   type Query {
     message: String
+    user(id: Int!): User
+  }
+
+  type Mutation {
+    users(id:Int, name: String, email: String): User
+  }
+
+  type User {
+    id: Int
+    name: String
+    email: String
   }
 `);
 
 // root resolver
 const root = {
   message: () => 'Hurray! The server has started',
+
+  users: (parent, args, context, info) => {
+    return users
+      .create(parent)
+      .then(user => {
+        return user;
+      })
+      .catch(error => {
+        return error;
+      })
+  },
+
+  user: (args) => {
+    return users.findById(args.id)
+    .then(user => {
+      return user;
+    })
+    .catch(error => {
+      return error;
+    })
+  },
+
 }
 
 // express server with graphql endpoint
